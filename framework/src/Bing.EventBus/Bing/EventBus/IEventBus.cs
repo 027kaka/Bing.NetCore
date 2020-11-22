b7e65d3ffcc8399a1b4040c62074a19c6952726b
jian玄冰 <jianxuanhuo1@126.com>
2020-11-23 00:37:08 +0800

    feat: 新增接口定义

---
+++ framework/src/Bing.EventBus/Bing/EventBus/IEventBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bing.EventBus
+{
+    /// <summary>
+    /// 事件总线
+    /// </summary>
+    public interface IEventBus
+    {
+    }
+}
---
+++ framework/src/Bing.EventBus/Bing/EventBus/IEventHandlerDisposeWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bing.EventBus
+{
+    /// <summary>
+    /// 事件处理器自动释放包装
+    /// </summary>
+    public interface IEventHandlerDisposeWrapper : IDisposable
+    {
+        /// <summary>
+        /// 事件处理器
+        /// </summary>
+        IEventHandler EventHandler { get; }
+    }
+}
